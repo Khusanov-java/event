dd94ca4bfe41208e09bc016d3ccedc5ce5f2cf0b
Khusanov-babulya <khusanovmirsaid@gmail.com>
2025-06-25 14:06:12 +0500

    asd

--- src/main/java/org/example/event/controller/ProfileController.java
+++ src/main/java/org/example/event/controller/ProfileController.java
@@ -1,14 +1,26 @@
 package org.example.event.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.event.entity.User;
+import org.example.event.repo.UserRepository;
 import org.example.event.utils.Util;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(Util.PROFILE_PATH)
 @RequiredArgsConstructor
 public class ProfileController {
 
+    private final UserRepository userRepository;
 
+    @GetMapping
+    public User profile(@RequestParam Long id) {
+        Optional<User> byId = userRepository.findById(id);
+        return byId.orElse(null);
+    }
 }
--- src/main/java/org/example/event/entity/User.java
+++ src/main/java/org/example/event/entity/User.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -22,6 +24,18 @@ public class User {
     private String email;
     private String password;
     private String profileImage;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_following",
+            joinColumns = @JoinColumn(name = "follower_id"),
+            inverseJoinColumns = @JoinColumn(name = "following_id")
+    )
+    private List<User> following;
+
+    @ManyToMany(mappedBy = "following")
+    private List<User> followers;
+
 
     @Enumerated(EnumType.STRING)
     private Role role = Role.USER;
